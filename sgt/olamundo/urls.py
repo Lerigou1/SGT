86c61b42e3d10990e686667c1ed77e5606795f88
Lerigou1 <leriston.souza09@aluno.ifce.edu.br>
2023-02-28 16:56:32 -0300

    HTML

--- sgt/olamundo/urls.py
+++ sgt/olamundo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="ola")
+    path("", views.index, name="ola"),
+    path("<str:nome>", views.saudacao, name="saudacao")
 ]
--- sgt/olamundo/views.py
+++ sgt/olamundo/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Olá mundo, Que tistreza!")
+    
+def saudacao(request, nome):
+    return render(request, 'index.html')
