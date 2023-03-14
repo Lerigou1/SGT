e8fa114b8059e2b7c1d34431b341a8006f2322e3
Lerigou1 <leriston.souza09@aluno.ifce.edu.br>
2023-03-14 13:53:41 -0300

    Formulários

--- sgt/olamundo/urls.py
+++ sgt/olamundo/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path("", views.index, name="ola"),
-    #path("<str:nome>", views.ola, name="oi"),
-    path("<str:name>", views.tiadozap, name="oi"),
-    #path("<str:nome>", views.saudacao, name="saudacao"),
+    path("<str:name>", views.tiadozap, name="oi")
 ]
